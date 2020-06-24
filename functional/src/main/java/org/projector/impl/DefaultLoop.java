f6fe390782d33171387980b72e63aede487c0fe5
Kirill Bogatikov <kirillbogatikoff@gmail.com>
2020-06-24 21:43:04 +0300

    DefaultLoop implementation of Loop interface

---
+++ functional/src/main/java/org/projector/impl/DefaultLoop.java
@@ -0,0 +1,34 @@
+package org.projector.impl;
+
+import org.projector.interfaces.Loop;
+
+public class DefaultLoop implements Loop {
+    public static final int ADD = 0, SKIP = 1, STOP = 2;
+
+    int index;
+    int action;
+
+    public void skip() {
+        action = SKIP;
+    }
+
+    public void stop() {
+        action = STOP;
+    }
+
+    public int index() {
+        return index;
+    }
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public int getAction() {
+		return action;
+	}
+
+	public void setAction(int action) {
+		this.action = action;
+	}
+}
