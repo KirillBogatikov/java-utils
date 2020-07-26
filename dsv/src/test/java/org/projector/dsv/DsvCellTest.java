1391cf5828bbe57a5772b7bfe554aaf1cee86b15
Kirill Bogatikov <kirillbogatikoff@gmail.com>
2020-07-26 20:39:23 +0300

    optimize DsvCellTest

--- dsv/src/test/java/org/projector/dsv/DsvCellTest.java
+++ dsv/src/test/java/org/projector/dsv/DsvCellTest.java
@@ -23,7 +23,6 @@ public class DsvCellTest {
 
     @Test
     public void testSet() {
-        table.clear();
         DsvCell cell = table.addCell(0, 0);
         cell.set("Hello, world!", v -> v.split(",")[0]);
         assertEquals("Hello", cell.getString());
@@ -31,7 +30,6 @@ public class DsvCellTest {
 
     @Test
     public void testGet() {
-        table.clear();
         DsvCell cell = table.addCell(0, 0);
         cell.setString("What's up?");
         assertEquals("What", cell.get(v -> v.split("'")[0]));
@@ -39,7 +37,6 @@ public class DsvCellTest {
 
     @Test
     public void testString() {
-        table.clear();
         DsvCell cell = table.addCell(0, 0);
         cell.setString("Hello, world!");
         assertEquals("Hello, world!", cell.getString());
@@ -47,7 +44,6 @@ public class DsvCellTest {
 
     @Test
     public void testInt() {
-        table.clear();
         DsvCell cell = table.addCell(0, 0);
         cell.setInt(15);
         assertEquals("15", cell.getString());
@@ -56,7 +52,6 @@ public class DsvCellTest {
 
     @Test
     public void testLong() {
-        table.clear();
         DsvCell cell = table.addCell(0, 0);
         cell.setLong(15123412421234L);
         assertEquals("15123412421234", cell.getString());
@@ -65,7 +60,6 @@ public class DsvCellTest {
 
     @Test
     public void testShort() {
-        table.clear();
         DsvCell cell = table.addCell(0, 0);
         cell.setShort(15123);
         assertEquals("15123", cell.getString());
@@ -74,7 +68,6 @@ public class DsvCellTest {
 
     @Test
     public void testDouble() {
-        table.clear();
         DsvCell cell = table.addCell(0, 0);
         cell.setDouble(15123.67123);
         assertEquals("15123.67123", cell.getString());
@@ -83,7 +76,6 @@ public class DsvCellTest {
 
     @Test
     public void testSetFloat() {
-        table.clear();
         DsvCell cell = table.addCell(0, 0);
         cell.setFloat(153.67f);
         assertEquals("153.67", cell.getString());
@@ -92,7 +84,6 @@ public class DsvCellTest {
 
     @Test
     public void testByte() {
-        table.clear();
         DsvCell cell = table.addCell(0, 0);
         cell.setByte((byte) 1);
         assertEquals("1", cell.getString());
@@ -101,7 +92,6 @@ public class DsvCellTest {
 
     @Test
     public void testBoolean() {
-        table.clear();
         DsvCell cell = table.addCell(0, 0);
         cell.setBoolean(true);
         assertEquals("true", cell.getString());
