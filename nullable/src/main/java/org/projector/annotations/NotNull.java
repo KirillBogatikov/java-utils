ee0044c53103d79e7926af8b856b4c0c2c000d5d
Kirill Bogatikov <kirillbogatikoff@gmail.com>
2020-10-23 21:58:35 +0300

    #4 Nullable and NotNull javaDoc

--- nullable/src/main/java/org/projector/annotations/NotNull.java
+++ nullable/src/main/java/org/projector/annotations/NotNull.java
@@ -1,5 +1,12 @@
 package org.projector.annotations;
 
+/**
+ * Requires parameter's value is not null.
+ * If some method's parameter marked as NotNull, it means that you need to expect a NullPointerException error when using <code>null</code> value.
+ * As a rule, such methods contains null checking and throws NullPointerException manually.
+ * 
+ * @author Kirill Bogatikov
+ */
 public @interface NotNull {
     String value() default "";
 
--- nullable/src/main/java/org/projector/annotations/Nullable.java
+++ nullable/src/main/java/org/projector/annotations/Nullable.java
@@ -1,5 +1,12 @@
 package org.projector.annotations;
 
+/**
+ * Allows parameter's value to be nullable.
+ * Values marked with this annotations requires checking for null values. 
+ * If some method's parameter marked as Nullable, it means that you don't need to expect a NullPointerException error when using <code>null</code> value.
+ * 
+ * @author Kirill Bogatikov
+ */
 public @interface Nullable {
     String value() default "";
 }
